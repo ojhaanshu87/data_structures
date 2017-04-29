643dafca4e7c539200a7482ae491512bad842730
ojhaanshu87 <ojhaanshu91@gmail.com>
2017-04-29 22:20:49 +0530

    longest palindrom sub-seq inside given string for odd and even length

---
+++ Dynamic_Programming_Archive/longest_palindrome_subseq.py
@@ -0,0 +1,42 @@
+def longest_palindrome(string_elem):
+    max_len = 1
+ 
+    start = 0
+    length = len(string_elem)
+ 
+    low = 0
+    high = 0
+ 
+    # One by one consider every character as center point of 
+    # even and length palindromes
+    for i in xrange(1, length):
+    # Find the longest even length palindrome with center
+    # points as i-1 and i.
+        low = i - 1
+        high = i
+        while low >= 0 and high < length and string_elem[low] == string_elem[high]:
+            if high - low + 1 > max_len:
+                start = low
+                max_len = high - low + 1
+            low -= 1
+            high += 1
+ 
+        # Find the longest odd length palindrome with center 
+        # point as i
+        low = i - 1
+        high = i + 1
+        while low >= 0 and high < length and string_elem[low] == string_elem[high]:
+            if high - low + 1 > max_len:
+                start = low
+                max_len = high - low + 1
+            low -= 1
+            high += 1
+ 
+    print "Longest palindrome substring is:",
+    print string_elem[start:start + max_len]
+ 
+    return max_len
+ 
+# # Driver program to test above functions
+# string = "forgeeksskeegfor"
+# print "Length is: " + str(longest_palindrome(string))
