270bb5a5b56c3c489c65254385441de22ebbd66d
ojhaanshu87 <ojhaanshu91@gmail.com>
2017-07-21 07:11:22 +0530

    max product sub array updated

--- Array_Archive/max_prod_subarray.py
+++ Array_Archive/max_prod_subarray.py
@@ -1,21 +1,29 @@
-def max_sub_array_prod(arr):
-    # max positive product ending at the current position
-    max_ending_here = 1
-    # min positive product ending at the current position
-    min_ending_here = 1
-    # Initialize maximum so far
-    max_so_far = 1
-    for i in range(0,len(arr)):
-        if arr[i] > 0:
-            max_ending_here = max_ending_here*arr[i]
-            min_ending_here = min (min_ending_here * arr[i], 1)
-        elif arr[i] == 0:
-            max_ending_here = 1
-            min_ending_here = 1
-        else:
-            temp = max_ending_here
-            max_ending_here = max (min_ending_here * arr[i], 1)
-            min_ending_here = temp * arr[i]
-        if (max_so_far <  max_ending_here):
-            max_so_far  =  max_ending_here
-    return max_so_far
+class Solution(object):
+	def __init__(self, arr):
+		self.arr = arr
+
+	def max_prod_subarray(self):
+		max_so_far = 1
+		max_end_here = 1
+		min_end_here = 1
+		for elem in range(0, len(self.arr)):
+			# if list elem > 0
+			if self.arr[elem] > 0:
+				max_end_here = max_end_here *self.arr[elem]
+				min_end_here = min(min_end_here * self.arr[elem], 1)
+			#if list elem equals 0
+			if self.arr[elem] == 0:
+				max_end_here =1
+				min_end_here =1
+			#if elem less than 0
+			else:
+				tmp = max_end_here
+				max_end_here = max(min_end_here * self.arr[elem], 1)
+				min_end_here = tmp * self.arr[elem]
+				if max_so_far < max_end_here:
+					max_so_far = max_end_here
+		return max_so_far
+
+#if __name__ == '__main__':
+#	solution = Solution([1, -2, -3, 0, 7, -8, -2])
+#	print solution.max_prod_subarray()
