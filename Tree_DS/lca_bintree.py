5ec1e7bd9dcb21422b235859d487e113fe493edb
ojhaanshu87 <ojhaanshu91@gmail.com>
2017-08-12 11:46:29 +0530

    lca updated

--- Tree_DS/lca_bintree.py
+++ Tree_DS/lca_bintree.py
@@ -1,17 +1,21 @@
-class Node:
-    def __init__(self,key):
-        self.left = None
-        self.right = None
-        self.val = key
+class Node(object):
+	def __init__(self, data):
+		self.val = data
+		self.left = None
+		self.right = None
 
-def lca_binary_tree(root, elem1, elem2):
-    if root is None:
-        return 0
-    if root.val==elem1 or root.val==elem2:
-        return root.val
-    left_lca = lca_binary_tree(root.left, elem1, elem2)
-    right_lca = lca_binary_tree(root.right, elem1, elem2)
-    if left_lca and right_lca:
-        return root.val
-    else:
-        return left_lca if left_lca else right_lca
+class Tree(object):
+	def __init__(self):
+		self.root = None
+
+	def lca_bintree(self, root, elem1, elem2):
+		if root is None:
+			return
+		if root.val == elem1 or root.val == elem2:
+			return root.val,
+		left_lca = self.lca_bintree(root.left, elem1, elem2)
+		right_lca = self.lca_bintree(root.right, elem1, elem2)
+		if left_lca and right_lca:
+			return root.val
+		else:
+			return left_lca if left_lca else right_lca
