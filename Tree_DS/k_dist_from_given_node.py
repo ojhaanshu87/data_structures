f6a129217b51906c996dbd438c48ca79f9119da3
ojhaanshu87 <ojhaanshu91@gmail.com>
2017-08-12 08:44:03 +0530

    distance from given nodeupdated

--- Tree_DS/k_dist_from_given_node.py
+++ Tree_DS/k_dist_from_given_node.py
@@ -1,55 +1,54 @@
-class Node:
-	def __init__(self, key):
+class Node(object):
+	def __init__(self, data):
+		self.val = data
 		self.left = None
 		self.right = None
-		self.val = key
 
-#k distance from given node Utility Function
-def k_dist_node_down(root, k):
-    if root is None or k <0:
-        return
-    if k==0:
-        print root.val
-        return
+class Tree(object):
+	def __init__(self):
+		self.root = None
 
-    k_dist_node_down(root.left, k-1)
-    k_dist_node_down(root.right, k-1)
+	def k_distance_down_from_root(self, root, k):
+		if root == None:
+			return
+		if k ==0:
+			print root.val,
+		self.k_distance_down_from_root(root.left, k-1)
+		self.k_distance_down_from_root(root.right, k-1)
 
-def k_distnace_from_given_node(root, k, target):
-    #Base Case 1
-    if root is None:
-        return -1
-    #Base Case 2 
-    # If target is root then print all downward nodes
-    if root==target:
-        k_dist_node_down(root, k)
-        return 0
-    # Recur for left subtree
-    left_recur = k_distnace_from_given_node(root.left, k, target)
-    # Check if target node was found in left subtree
-    if left_recur != -1:
-        # If root is at distance k from target, print root
-        # Note: left_recur is distance of root's left child from target
-        if left_recur+1 == k:
-            print root.val
-        # Else go to right subtreee and print all k-left_recur-2 distant nodes
-        # Note: that the right child is 2 edges away from left chlid
-        else:
-            k_dist_node_down(root.right, k-left_recur-2)
-        # Add 1 to the distance and return value for for parent calls
-        return 1+left_recur
-    
-    # Similar Recur for right subtree (as last 26-39 line number for left subtree)
-    right_recur = k_distnace_from_given_node(root.right, k, target)
-    if right_recur !=-1:
+	def k_distance_from_given_node(self, root, k, given_node):
+		if root is None:
+			return
+		if root == given_node:
+			self.k_distance_down_from_root(root, k)
+			return 0
+		#recur for left subtree
+		left_recur = self.k_distance_from_given_node(root.left, k, given_node)
+		#CHECK IF GIVEn node found in left subtree
+		if left_recur != -1:
+			#if root is distance from given node, print root
+			#left recur is distanc of root's left child of target
+			if left_recur+1 == k:
+				print root.val,
+			else:
+				self.k_distance_down_from_root(root.right, k-left_recur-2)
+			return 1 + left_recur
+		#mirror of line 25 to line 35 above
+		right_recur = self.k_distance_from_given_node(root.right, k, given_node)
+		if right_recur != -1:
+			if right_recur+1 == k:
+				print root.val,
+			else:
+				self.k_distance_down_from_root(root.left, k-right_recur-2)
+			return 1 + right_recur
 
-        if right_recur+1 ==k:
-
-            print root.val
-
-        else:
-            k_dist_node_down(root.left, k-right_recur-2)
-
-        return 1+right_recur
-    # If target was neither present in left nor in right subtree    
-    return -1
+#Time Complexity:O(n)	
+# tree_obj = Tree()
+# tree_obj.root = Node(1)
+# tree_obj.root.left = Node(2)
+# tree_obj.root.right = Node(3)
+# tree_obj.root.left.left = Node(7)
+# tree_obj.root.left.right = Node(6)
+# tree_obj.root.right.left = Node(5)
+# tree_obj.root.right.right = Node(4)
+# tree_obj.k_distance_from_given_node(tree_obj.root, 2, tree_obj.root.left.left)
