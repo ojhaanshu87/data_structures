2b4511da0c251502a29c93702e43ec707bddf100
ojhaanshu87 <ojhaanshu91@gmail.com>
2017-12-16 08:44:54 +0530

    return two string is anagram or not

--- String_Archive/anagram_or_not.py
+++ String_Archive/anagram_or_not.py
@@ -1,26 +1,23 @@
-MAX_SIZE = 256
- 
-def are_anagram(string1, string2):
+class Solution(object):
+  def __init__(self, str1, str2):
+    self.str1 = str1
+    self.str2 = str2
 
-	#base case
-    if len(string1) != len(string2):
-        return 0
- 
-    #two count arrays and initialize all values as 0
-    count1 = [0] * MAX_SIZE
-    count2 = [0] * MAX_SIZE
- 
-    # For each character in input strings, increment count
-    # in the corresponding count array
-    for i in string1:
-        count1[ord(i)]+=1
- 
-    for i in string2:
-        count2[ord(i)]+=1
- 
-    # Compare count arrays
-    for i in xrange(MAX_SIZE):
-        if count1[i] != count2[i]:
-            return 0
- 
-    return 1
+  def is_anagram(self):
+    idx1 = []
+    idx2 = []
+    sorted_str1, sorted_str2 = sorted(self.str1.lower()), sorted(self.str2.lower())
+    #base case1
+    if len(sorted_str1) <> len(sorted_str2):
+      return False
+    #base case2
+    elif len(sorted_str1) is None or len(sorted_str2) is None:
+      return False
+    #convert into ord(sets) and compare sum of array
+    else:
+      for elem1 in sorted_str1:
+        idx1.append(ord(elem1) - ord('a'))
+      for elem2 in sorted_str2:
+        idx2.append(ord(elem2) - ord('a'))
+    if sum(idx1) == sum(idx2):
+      return True
